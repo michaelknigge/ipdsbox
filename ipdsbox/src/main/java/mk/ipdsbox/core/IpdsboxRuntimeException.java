df64cddccaf49b9211221f0b80ac6a98d95938fc
michaelknigge <michael.knigge@gmx.de>
2016-03-28 19:54:31 +0200

    Removed unused method from IpdsRuntimeException

--- ipdsbox/src/main/java/mk/ipdsbox/core/IpdsboxRuntimeException.java
+++ ipdsbox/src/main/java/mk/ipdsbox/core/IpdsboxRuntimeException.java
@@ -16,15 +16,4 @@ public final class IpdsboxRuntimeException extends RuntimeException {
     public IpdsboxRuntimeException(final String message) {
         super(message);
     }
-
-    /**
-     * Constructs a new {@link IpdsboxRuntimeException} with the specified detail message and
-     * nested {@link Throwable}.
-     *
-     * @param message the detail message.
-     * @param e the nested {@link Throwable}.
-     */
-    public IpdsboxRuntimeException(final String message, final Throwable e) {
-        super(message, e);
-    }
 }
