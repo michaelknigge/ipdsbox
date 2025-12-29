7686ffef54b118e6781adb826733cd31bcc3ffc1
Michael Knigge <michael.knigge@set.de>
2025-12-29 02:32:22 +0100

    Removed comment.

--- ipdsbox/src/main/java/de/textmode/ipdsbox/io/IpdsByteArrayOutputStream.java
+++ ipdsbox/src/main/java/de/textmode/ipdsbox/io/IpdsByteArrayOutputStream.java
@@ -20,11 +20,6 @@ public final class IpdsByteArrayOutputStream {
 
     private final ByteArrayOutputStream baos;
 
-    // TODO: maybe we should implement static "createForXxxx" and "buildXxxx" Methods (like
-    //    "createForTriplet" and "buildTriplet" that perform specific tasks (i. e. for
-    //    Triplets the create-method sets length to 0, sets the given TripletID and the
-    //    "build"-Method then calculates and sets the lengths in the first byte...
-
     /**
      * Constructs an {@link IpdsByteArrayInputStream}.
      */
